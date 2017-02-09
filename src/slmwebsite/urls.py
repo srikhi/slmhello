b7daa6ba5c009fa80ae42b3ac5b7adb7d36a782b
Sandeep Rikhi <sandeep.rikhi@rubrik.com>
2017-02-09 23:38:45 +0000

    Some more hellos

--- src/slmwebsite/urls.py
+++ src/slmwebsite/urls.py
@@ -1,4 +1,4 @@
-"""slmwebapp URL Configuration
+"""slmwebsite URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/1.10/topics/http/urls/
@@ -15,8 +15,9 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from helloslmapp.views import HomePageView
 
 urlpatterns = [
-    url(r'', helloslm.urls),
+    url(r'^$', HomePageView),
     url(r'^admin/', admin.site.urls),
 ]
